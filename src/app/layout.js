0284bab486f43f9fda67fbd4686575a2dedb34af
vladimir <vladimirmuratov@mail.ru>
2026-01-29 10:05:49 +0300

    refresh yandex webmaster

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,18 +12,18 @@ import './globals.css';
 const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
-    metadataBase: new URL('https://cirrhosis.mskdoctor.ru'),
+    metadataBase: new URL('https://msk-cirrhosis.vercel.app'),
     title: 'Лечение цирроза печени — наблюдение, терапия и поддержка печени',
     description: 'Комплексное лечение цирроза печени под наблюдением гепатолога. Диагностика, терапия, контроль осложнений. Индивидуальный подход.',
     keywords: ['лечение цирроза печени, платная госпитализация цирроз, лечение печени Москва, гастроэнтеролог цирроз, цирроз печени терапия, помощь при циррозе'],
     other: {
-        ['yandex-verification']: '63b15477d46dd1f6',
+        ['yandex-verification']: '6e7ff401f8c4e451',
     },
     openGraph: {
         title: 'Лечение цирроза печени в Москве | Госпитализация и терапия. Звоните! 8 (499) 719-81-00, 24/7',
         description: 'Лечение цирроза печени в Москве. Госпитализация в федеральные и ведомственные клиники. Современная диагностика и терапия. Приём пациентов из регионов и СНГ.',
         images: ['/images/banner7.webp'],
-        url: 'https://cirrhosis.mskdoctor.ru',
+        url: 'https://msk-cirrhosis.vercel.app',
         type: 'website',
         locale: 'ru_RU',
     }
